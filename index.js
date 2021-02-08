fe60c23813ec10138527fcbdc74ff326e91997d7
Adrienne Yao <adrienne.yao@icloud.com>
2021-02-08 16:06:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ class Lunch {
 
 class Dinner {
   constructor(salad, soup, entree, dessert) {
-    this.salad = salad 
-    this.soup = soup 
-    this.entree = entree 
+    this.salad = salad
+    this.soup = soup
+    this.entree = entree
     this._dessert = dessert
   }
 }
